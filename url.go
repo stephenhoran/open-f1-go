a986cbd72f16c36101a8df91cc5323e1e9cf33cf
Steve Horan <horan116@gmail.com>
2025-04-12 13:39:09 -0400

    Refactor

--- url.go
+++ url.go
@@ -1,5 +1,10 @@
 package openf1go
 
+import (
+	"io"
+	"net/http"
+)
+
 type Arg struct {
 	Key   string
 	Value string
@@ -25,4 +30,19 @@ func ArgBuilder(args []Arg) string {
 	}
 
 	return url
+}
+
+func GetHTTPRequest(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, err
 }
